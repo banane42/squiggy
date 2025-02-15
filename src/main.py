4b88b0a21c1d4dc7b562a38e480353dfbf016281
gjmorris <griffenmorrison@yahoo.com>
2025-02-15 17:26:43 -0600

    datasheet data loader

--- src/main.py
+++ src/main.py
@@ -1,33 +1,29 @@
 import csv
 import requests
 
-b = "false"
-con_b = bool(b)
-print(con_b)
+print("\n\n\n+++++++++++++++++++++++++\nBegin Local CSV process\n+++++++++++++++++++++++++\n\n\n")
 
-# print("\n\n\n+++++++++++++++++++++++++\nBegin Local CSV process\n+++++++++++++++++++++++++\n\n\n")
+with open('/Users/gjmorris/Downloads/wahapedia_export/Datasheets_models.csv', 'r') as csvfile:
+	reader = csv.reader(csvfile, delimiter='|')
+	for row in reader:
+		print("--------------------------")
+		col_str = ""
+		for column in row:
+			col_str += str(column) + "+"
+		print(col_str)
 
-# with open('/Users/gjmorris/Downloads/wahapedia_export/Datasheets_models.csv', 'r') as csvfile:
-# 	reader = csv.reader(csvfile, delimiter='|')
-# 	for row in reader:
-# 		print("--------------------------")
-# 		col_str = ""
-# 		for column in row:
-# 			col_str += str(column) + "+"
-# 		print(col_str)
+print("\n\n\n+++++++++++++++++++++++++\nBegin HTTP Request CSV Process\n+++++++++++++++++++++++++\n\n\n")
 
-# print("\n\n\n+++++++++++++++++++++++++\nBegin HTTP Request CSV Process\n+++++++++++++++++++++++++\n\n\n")
+response = requests.get(
+	url='http://wahapedia.ru/wh40k10ed/Datasheets_models.csv'
+)
 
-# response = requests.get(
-# 	url='http://wahapedia.ru/wh40k10ed/Datasheets_models.csv'
-# )
+decoded_content = response.content.decode('utf-8')
 
-# decoded_content = response.content.decode('utf-8')
-
-# reader = csv.reader(decoded_content.splitlines(), delimiter='|')
-# for row in reader:
-# 	print("--------------------------")
-# 	col_str = ""
-# 	for column in row:
-# 		col_str += str(column) + "+"
-# 	print(col_str)
+reader = csv.reader(decoded_content.splitlines(), delimiter='|')
+for row in reader:
+	print("--------------------------")
+	col_str = ""
+	for column in row:
+		col_str += str(column) + "+"
+	print(col_str)
